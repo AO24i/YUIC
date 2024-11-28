cf9842109c8abdf6f8ad25becf93e7215639fecc
AO™ <anthony@osawere.com>
2024-11-28 11:48:55 +0100

    Create form.blade.php

---
+++ zero/form.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'submit' => 'save',
+    'action' => null,
+    'id' => null,
+    'module' => null,
+
+    'actions' => 'create|update|clone',
+])
+<form wire:submit.prevent="{{ $submit }}">
+	@csrf
+
+	<x-yale.wire.form.notify actions="{{ $actions }}" />
+
+	<div class="row mb-6">
+		{{ $slot }}
+	</div>
+
+	<div class="row mt-5">
+		<div class="d-flex justify-content-center align-items-middle">
+			@if ($action === 'create')
+				<x-yale.wire.btn.save label='create' :moduleX=$module :submit="$submit" />
+				<x-yale.wire.btn.reset.create :module="$module" :submit="$submit" />
+			@elseif($action === 'update')
+				<x-yale.wire.btn.save label='save' :moduleX=$module :submit="$submit" :id="$id" />
+				<x-yale.wire.btn.reset.update :module="$module" :submit="$submit" :id="$id" />
+			@elseif($action === 'clone')
+				<x-yale.wire.btn.save label='save as' :moduleX=$module :submit="$submit" />
+				<x-yale.wire.btn.reset.update :module="$module" :submit="$submit" :id="$id" />
+			@endif
+		</div>
+	</div>
+</form>
