11e17880640d06e20b3e7a9f72d924b7f9b533c9
AO™ <anthony@osawere.com>
2024-11-28 11:48:39 +0100

    -

--- odao/notify.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['theme' => 'bootstrap'])
-
-@if (Session::has('successX'))
-	<x-yuic.wire.notify.success message="{{ Session::get('successX') }}" :theme=$theme />
-@elseif ($errors->any())
-	<x-yuic.wire.notify.error :errors=$errors :theme=$theme />
-@endif
