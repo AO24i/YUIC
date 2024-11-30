6a1202d394b3c9dabf274a57255ecc4e040f17db
AO™ <anthony@osawere.com>
2024-11-30 19:35:08 +0100

    Update block.blade.php

--- zero/form/block.blade.php
+++ zero/form/block.blade.php
@@ -1,12 +1,11 @@
 @props([
     'submit' => 'save',
     'action' => null,
-    'id' => null,
+    'rowID' => null,
     'module' => null,
-
     'actions' => 'create|update|clone',
 ])
-<form wire:submit.prevent="{{ $submit }}">
+<form wire:submit.prevent="{{ $submit }}('{{ $action }}', '{{ $rowID }}')">
 	@csrf
 
 	<x-yale.zero.notify actions="{{ $actions }}" />
@@ -21,11 +20,11 @@
 				<x-yale.zero.button.save label='create' :moduleX=$module :submit="$submit" />
 				<x-yale.zero.button.reset.create :module="$module" :submit="$submit" />
 			@elseif($action === 'update')
-				<x-yale.zero.button.save label='save' :moduleX=$module :submit="$submit" :id="$id" />
-				<x-yale.zero.button.reset.update :module="$module" :submit="$submit" :id="$id" />
+				<x-yale.zero.button.save label='save' :moduleX=$module :submit="$submit" :action="$action" />
+				<x-yale.zero.button.reset.update :module="$module" :submit="$submit" :rowID="$rowID" />
 			@elseif($action === 'clone')
-				<x-yale.zero.button.save label='save as' :moduleX=$module :submit="$submit" />
-				<x-yale.zero.button.reset.update :module="$module" :submit="$submit" :id="$id" />
+				<x-yale.zero.button.save label='save as' :moduleX=$module :submit="$submit" :action="$action" />
+				<x-yale.zero.button.reset.update :module="$module" :submit="$submit" :rowID="$rowID" />
 			@endif
 		</div>
 	</div>
