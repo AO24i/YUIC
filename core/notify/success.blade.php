01a74501b50f6d8f7d69a129fe78cd96970e59e6
AO™ <anthony@osawere.com>
2024-11-30 19:34:57 +0100

    Update success.blade.php

--- core/notify/success.blade.php
+++ core/notify/success.blade.php
@@ -1,4 +1,4 @@
-@props(['message', 'theme' => 'bootstrap'])
+@props(['message', 'theme' => 'bootstrap', 'fade' => true])
 
 @php
     $css = $theme === 'bootstrap' ? 'alert alert-success' : ($theme === 'tailwind' ? 'text-green-600' : '');
